cd2642dcb760fe1d404ff2e13ecf7bdb39bdb27b
Pahrul <ahmdphrl1st14@gmail.com>
2021-05-25 23:08:56 +0800

    Penambahan Database, Interface Note Dao, dan Note

--- app/src/main/java/com/example/roomdb/ExampleInstrumentedTest.kt
+++ app/src/main/java/com/example/roomdb/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("id.ac.unhas.aplikasibelajar", appContext.packageName)
+        assertEquals("example.roomdb", appContext.packageName)
     }
 }
